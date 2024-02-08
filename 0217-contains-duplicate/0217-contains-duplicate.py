51b1322567ee17f7573631addd2660590b650570
benyas1getachew <99409114+benyas1getachew@users.noreply.github.com>
2024-02-09 01:29:28 +0530

    Time: 432 ms (51.08%), Space: 32 MB (72.65%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        sets=set(nums)
+        if len(sets)< len(nums):
+            return True
+        else:
+            return False
+        
