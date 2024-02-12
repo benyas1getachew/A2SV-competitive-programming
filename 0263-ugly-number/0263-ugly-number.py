2ad0b91e006d6c634b63b0f93ae0cc0b6f0f894c
benyas1getachew <99409114+benyas1getachew@users.noreply.github.com>
2024-02-12 23:34:24 +0530

    Time: 36 ms (64.27%), Space: 16.6 MB (58.38%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.py
+++ 0263-ugly-number/0263-ugly-number.py
@@ -2,7 +2,7 @@ class Solution:
     def isUgly(self, n: int) -> bool:
         if n <= 0:
             return False
-        for prime in [2, 3, 5]:
+        for prime in [5,3,2]:
             while n % prime == 0:
                 n //= prime
         return n == 1
