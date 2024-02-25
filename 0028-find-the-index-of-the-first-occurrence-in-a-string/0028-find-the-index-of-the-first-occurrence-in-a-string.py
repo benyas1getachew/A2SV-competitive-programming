66a406942340e7adcab04d44280a0cc4f001edc0
benyas1getachew <99409114+benyas1getachew@users.noreply.github.com>
2024-02-25 23:24:04 +0530

    Time: 39 ms (34.15%), Space: 16.5 MB (94.71%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -3,7 +3,7 @@ class Solution:
         n=len(haystack)
         k=len(needle)
         for a in range(n):
-            if haystack[a]==needle[0]:
-                if haystack[a:a+k]==needle:
-                    return a
+            if haystack[a]==needle[0] and haystack[a:a+k]==needle:
+                
+                return a
         return -1
