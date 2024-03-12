48da669af92de76f743f219f9073c02e72a651f1
benyas1getachew <99409114+benyas1getachew@users.noreply.github.com>
2024-03-12 15:59:13 +0530

    Time: 109 ms (40.18%), Space: 17.1 MB (36.05%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -4,9 +4,8 @@ class Solution:
         for i in range(len(heights)):
             dicts[heights[i]]=names[i]
         dicts=dict(sorted(dicts.items(),reverse = True))
-        k=0
+        arr=[]
         for i in dicts:
-            names[k]=(dicts[i])
-            k+=1
-        return names
+            arr.append(dicts[i])
+        return arr
             
